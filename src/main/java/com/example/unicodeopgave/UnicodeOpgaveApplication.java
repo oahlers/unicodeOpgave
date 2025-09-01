8b14dca61910f772212b361de6406c4f59d567df
oahlers <oliv2527@stud.kea.dk>
2025-09-01 13:37:30 +0200

    UnicodeJPAOpgave1

---
+++ src/main/java/com/example/unicodeopgave/UnicodeOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.unicodeopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicodeOpgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicodeOpgaveApplication.class, args);
+    }
+
+}
